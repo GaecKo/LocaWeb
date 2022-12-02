13556de469aa3e3014b7bc66517e182a55622f86
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2022-12-02 01:32:07 +0100

    comment recursive func

--- site/database.js
+++ site/database.js
@@ -1,5 +1,5 @@
 const { Sequelize, DataTypes, Model} = require('sequelize');
-
+const util = require('util')
 const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: "./data/database.sqlite"  
@@ -55,6 +55,10 @@ Ad.init({
         allowNull: true,
         defaultValue: 0
     },
+    comments: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
     user: {
         type: DataTypes.TEXT,
         allowNull: false,
@@ -105,7 +109,6 @@ Comment.init({
 // To sync the database, if changes are done in the above init functions, uncomment next line. Be carefull, it's maybe needed to delete database content
 // sequelize.sync()
 
-
 async function getUser(username){
     /*
     * Return user (object with user's info) if finded, false if not
@@ -135,10 +138,10 @@ async function addUser(username, email, password) {
         email: email,
         password: password
     }).then(user => {
-        console.log("User added " + user);
+        console.log("User added: " + user);
         return true;
     }).catch(err => {
-        console.log("User already exists " + err);
+        console.log("User already exists?: " + err);
         return false;
     })
 }
@@ -154,10 +157,10 @@ async function setModoState(username, state) {
     }
     return User.update({moderator: state}, {where: {username: username}}).then(state => {
         if (state == 1) {
-            console.log("User :" + username + " has been updated to modo: " + state)
+            console.log("User: " + username + " has been updated: " + state)
             return true
         } else {
-            console.log("User couldn't be updated (not found)")
+            console.log("User couldn't be updated (not found).")
             return false
         }
     }).catch(err => {
@@ -193,6 +196,10 @@ async function getAllUsers() {
 }
 
 async function isModo(username) {
+    /*
+    *  Return true if User with username is modo
+    *  return false if not
+    */
     return User.findOne({where: {username: username, moderator: true}}).then(user => {
         if (user) {
             console.log(username + " is modo")
@@ -206,6 +213,140 @@ async function isModo(username) {
         return false
     })
 }
+
+async function getAuthors(JSONComment) {
+    let Author_list = []
+    while (JSONComment.response != false) {
+        break
+    }
+}
+
+async function getAd(adId) {
+    /*
+    *  Return an object: {id, desc, title, reports, comments, userId}
+    *  return false if id doesnt correspond to any known ad
+    */
+    return Ad.findOne({where: {id: adId}}).then( ad => {
+        if (ad) {
+            console.log("ad: " + adId + " was found and retrieved.")
+            ad = ad.dataValues
+            ad.comments = JSON.parse(ad.comments)
+            return ad
+        } else {
+            console.log("ad: " + adId + " was NOT found.")
+            return false
+        }
+    }
+    ).catch( err => {
+        console.log("Error while retrieving ad: " + err)
+        return false
+    })
+}
+
+async function addAd(description, title, userId) {
+    return Ad.create({
+        description: description,
+        title: title,
+        comments: JSON.stringify({comments: null}),
+        user: userId
+    }).then(ad => {
+        console.log('Ad added: ' + ad)
+        return true
+    }).catch(err => {
+        console.log("Error while adding Ad: " + err)
+    })
+}
+
+function RecAddComment(comments, parentId, id, content) {
+    if (parentId in comments) {
+        comments[parentId][id] = content
+        return [true, comments]
+    } 
+    for (key1 in comments) {
+        // console.log(key1)
+        let comment_key = null;
+        let in_com = comments[key1]
+        for (key2 in in_com) {
+            if (typeof in_com[key2] == 'object') {
+                comment_key = key2
+            }
+        }
+        if (comment_key != null) {
+            let [found, temp] = RecAddComment(comments[key1], parentId, id, content)
+            if (found) {
+                comments[key1] = temp
+                
+            }
+        
+     }
+}
+    return [false, comments]
+}
+
+function main() {
+    let a = 
+    {
+        1: {
+            text: "text1",
+            author: "author1",
+            3: {
+                text: "text3", 
+                author: "author3",
+                6: {
+                    text: "text6",
+                    author: "author6"
+                }
+            }
+        },
+        2: {
+            text: "text",
+            author: "author2",
+            8: {
+                9: {
+                    10:{
+                        text: "hello"
+                    }
+                }
+            }
+        }
+    }
+    a  = RecAddComment(a, 10, "NEW", {text: "WOWOWOW", author: "HEHEHEH"})[1]
+    //console.log(JSON.stringify(a))
+    console.log(util.inspect(a, {showHidden: false, depth: null}))
+    
+    
+}
+
+async function addComment(adId, cId, text, author, parentId=null) {
+    let ad = await getAd(adId)
+    let comments = ad.comments
+    content = {
+        text: text,
+        author: author
+    }
+    if (parentId == null) {
+        comments.cId = content
+    } else {
+        
+    }
+    return Ad.update({comments: comments}, {where: {id: adId}}).then(state => {
+        if (state == 1) {
+            console.log("Ad: " + adId + " has been updated.")
+            return true
+        } else {
+            console.log("Ad: " + adId + " couldn't be updated (not found).")
+            return false
+        }
+    }).catch(err => {
+        console.log("unable to add comment: " + err)
+        return false
+    })
+
+    
+}
+
+
+main()
 
 module.exports = {
     getUser,
