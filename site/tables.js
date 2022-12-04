bf3260876c7ac35dc4053c4471be722cb07c65cd
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2022-12-04 20:38:52 +0100

    added tables.js so sequelize tables are in a distinct file

---
+++ site/tables.js
@@ -0,0 +1,150 @@
+const { Sequelize, DataTypes, Model} = require('sequelize');
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./data/database.sqlite"  
+})
+
+class User extends Model {}
+
+class Ad extends Model {}
+
+class Comment extends Model {}
+
+// class Images extends Model {}
+
+User.init({
+    username: {
+        unique: true,
+        type: DataTypes.TEXT,
+        primaryKey: true,
+        allowNull: false
+    },
+    email: {
+        unique: true,
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    moderator: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: false
+    },
+    total_report: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+    }
+}, {sequelize, modelName: 'User'})
+
+Ad.init({
+    id : {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    city: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
+    reports: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+    },
+    comments: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    visibility: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: true
+    },
+    user: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        references: {
+            model: User,
+            key: "username"
+        }
+    },
+    images: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    }
+    
+    // address: pas obligatoire | images: pas obligatoire 
+      // prix: type (échange-€/h-...) + text correspondant | 
+      // signalement: à lier avec l'utilisateur
+      // ... à implémenter au fur et à mesure
+
+}, {sequelize, modelName: 'Ad'})
+
+Comment.init({
+    id : {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+    },
+    content : {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    reports : {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+    },
+    user : {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        references: {
+            model: User,
+            key: "username"
+        }
+    },
+    visibility: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: true
+    },
+    ad : {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Ad,
+            key: "id"
+        }
+    }
+}, {sequelize, modelName: 'Comment'})
+
+// To sync the database, if changes are done in the above init functions, uncomment next line. Be carefull, it's maybe needed to delete database content
+// sequelize.sync({force: true})
+
+
+module.exports = {
+    User,
+    Ad,
+    Comment
+}
