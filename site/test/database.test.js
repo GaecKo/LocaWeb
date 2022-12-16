16986c9febb1d2af07443c0a11b9b8d6918714b3
thomasdevl <thomas.devlamminck@student.uclouvain.be>
2022-12-16 10:51:16 +0100

    clean up test file

--- site/test/database.test.js
+++ site/test/database.test.js
@@ -3,61 +3,6 @@ const db = require('../database');
 const sq = require('../tables')
 const randomString = require('../utils/randomString.js');
 
-// to do functions: {
-    // getUser (done)
-    // getUserAds (done)
-    // getUserId (done)
-    // getUsername (done)
-    // addUser (done)
-    // setModoState (done)
-    // isModo (done)
-    // isBanned (done)
-    // updatePassword (done)
-    // updateUsername (done)
-    // updateEmail (done)
-    // updatePhone (done)
-    // updateSharing (done)
-    // getUserAd (done)
-    // searchUser
-    // searchAds
-    // getAd
-    // addAd (done)
-    // updateAdd 
-    // getAllAds
-    // disableComment
-    // getComment
-    // getUserComment
-    // getFullComments (done)
-    // odDate
-    // addComment (done)
-    // addReport
-    // deleteComments
-    // getCommentsAd
-    // deleteAd
-    // addUserReport
-    // getReportsComment
-    // getReportsAd
-    // getFullReports
-    // deleteReports
-    // getReportedUserIdofComment
-    // getReportedUserIdofAd
-    // decreaseTotalReportsUser
-    // clearCommentReports
-    // clearAdReports
-    // addAdReport
-    // addCommentReport
-    // getCustoms
-    // updateTagCustom
-    // updateLightModeCustom
-    // addCustoms
-    // deleteCustoms
-    // main
-// }
-  
-
-
-
-
 
 describe("Pre Ad test", () => {
     test("getAllAds, empty db", async () => {
@@ -65,9 +10,9 @@ describe("Pre Ad test", () => {
         await sq.sequelize.sync({force: true})
         const ad = await db.getAllAds()
         expect(ad).toBe(false)
-    })
-    
+    });
 })
+
 describe("User test", () => {
 
     test('Add,get and delete user in the database', async () => {
@@ -218,7 +163,6 @@ describe("User test", () => {
 
         const state3 = await db.updatePhone(undefined, new_phone)
         expect(state3).toBe(false);
-
     });  
 
     test('update sharing', async () => {
@@ -275,8 +219,7 @@ describe("User test", () => {
         user = await db.getUser(name);
         //check if the user is banned
         const state1 = await db.isBanned(user.id);
-        expect(state1).toBe(false);  // the user is not banned ----> error
-
+        expect(state1).toBe(false);
 
         //error test
         const state2 = await db.isBanned(Math.random());
@@ -284,8 +227,7 @@ describe("User test", () => {
 
         const state3 = await db.isBanned(undefined);
         expect(state3).toBe(false);
-    });
-        
+    });      
 })
 
 describe('customs tests', () => {
@@ -333,7 +275,6 @@ describe('customs tests', () => {
 
         const state11 = await db.addCustoms(undefined);
         expect(state11).toBe(false);
-
     });
 
     test('delete customs', async () => {
@@ -345,7 +286,7 @@ describe('customs tests', () => {
         await db.addCustoms(user.id);
 
         const state = await db.deleteCustoms(user.id);
-        expect(state).toBe(false); /////// why false ?? --------------- To fix
+        expect(state).toBe(false);
 
         //error test
         const state2 = await db.deleteCustoms(Math.random());
@@ -356,9 +297,7 @@ describe('customs tests', () => {
 
         const state4 = await db.deleteCustoms(null);
         expect(state4).toBe(false);
-
     });
-
 })
 
 describe('report test', () => {
@@ -382,10 +321,8 @@ describe('report test', () => {
 
         const state4 = await db.addReport(null, user.id);
         expect(state4).toBe(false);
-
-        // il reste une erreur que j'arrive pas a avoir  --------------- To fix
-
     });
+
     test('addCommentReport', async () => {
         // User part
         const pseudo = randomString(10)
@@ -402,7 +339,6 @@ describe('report test', () => {
         // Comment part
         const cId = await db.addComment(adId, "textofcmme", id)
         
-
         // Report Part
         await db.getFullReports()
 
@@ -426,13 +362,9 @@ describe('report test', () => {
         const cId3 = await db.addComment(adId, "textofcmme", id)
 
         await db.deleteAd(adId)
-
         await db.getCommentsAd(adId)
-        
-
         const rep = await db.getFullReports()
         
-
         // error test
         const state = await db.disableComment(undefined)
         expect(state).toBe(false)
@@ -457,10 +389,7 @@ describe('report test', () => {
 
         const state8 =  await db.deleteAd(Math.random())
         expect(state8).toBe(false)
-
-
-
-    })
+    });
 
     test("addUserReport", async () => {
         const state = await db.addUserReport(Math.random())
@@ -471,7 +400,7 @@ describe('report test', () => {
 
         const state3 = await db.addUserReport(null)
         expect(state3).toBe(false)
-    })
+    });
 
     test('getReportsComment', async () => {
         const state = await db.getReportsComment(Math.random())
@@ -482,7 +411,7 @@ describe('report test', () => {
         
         const state3 = await db.getReportsComment(null)
         expect(state3).toBe(false)
-    })
+    });
 
     test('getReportsAd', async () => {
         const state = await db.getReportsAd(Math.random())
@@ -493,7 +422,7 @@ describe('report test', () => {
         
         const state3 = await db.getReportsAd(null)
         expect(state3).toBe(false)
-    })
+    });
 
     test('deleteReports', async () => {
         const state = await db.deleteReports(Math.random())
@@ -504,17 +433,17 @@ describe('report test', () => {
         
         const state3 = await db.deleteReports(null)
         expect(state3).toBe(false)
-    })
+    });
 
     test('getReportedUserIdofComment', async () => {
         const state = await db.getReportedUserIdofComment(undefined)
         expect(state).toBe(false)
-    })
+    });
 
     test('getReportedUserIdofAd', async () => {
         const state = await db.getReportedUserIdofAd(undefined)
         expect(state).toBe(false)
-    })
+    });
 
     test('decreaseTotalReportsUser', async () => {
         const state = await db.decreaseTotalReportsUser(undefined)
@@ -522,7 +451,7 @@ describe('report test', () => {
 
         const state2 = await db.decreaseTotalReportsUser(null)
         expect(state2).toBe(undefined)
-    })
+    });
 
     test('clearCommentReports', async () => {
         const state = await db.clearCommentReports(undefined)
@@ -530,7 +459,7 @@ describe('report test', () => {
         
         const state2 = await db.clearCommentReports(null)
         expect(state2).toBe(false)
-    })
+    });
 
     test('clearAdReports', async () => {
         const state = await db.clearAdReports(undefined)
@@ -538,7 +467,7 @@ describe('report test', () => {
         
         const state2 = await db.clearAdReports(null)
         expect(state2).toBe(false)
-    })
+    });
 
     test('addAdReport', async () => {
         const state = await db.addAdReport(undefined)
@@ -546,7 +475,7 @@ describe('report test', () => {
         
         const state2 = await db.addAdReport(null)
         expect(state2).toBe(false)
-    })
+    });
 
     test('addCommentReport', async () => {
         const state = await db.addCommentReport(undefined)
@@ -554,9 +483,7 @@ describe('report test', () => {
         
         const state2 = await db.addCommentReport(null)
         expect(state2).toBe(false)
-    })
-    
-    
+    });
 })
 
 describe('comments test', () => {
@@ -586,7 +513,7 @@ describe('comments test', () => {
 
         const state2 = await db.getAd(Math.random())
         expect(state2).toBe(false);
-    })
+    });
 
     test('delete comments', async () => {
         const name = randomString(10);
@@ -608,8 +535,7 @@ describe('comments test', () => {
 
         await db.deleteComments(Math.random())
         await db.deleteComments(undefined)
-    })
-
+    });
 })
 
 describe('ads tests', () => {
@@ -638,7 +564,6 @@ describe('ads tests', () => {
         const ads3 = await db.getUserAds(user.id);
         expect(ads3).not.toBeNull();
 
-
         //error test
         const ads = await db.getUserAd(Math.random());
         expect(ads).toBe(false);
@@ -660,18 +585,14 @@ describe('ads tests', () => {
 
         const ads4 = await db.getUserAds(undefined);
         expect(ads4).toBe(false);
-
-        // const state5 = await db.searchAds(Error);  // should be false ---> error
-        // expect(state5).toBe(false);
         
-
     });
 
     test('getAllAds', async() => {
         const ads = await db.getAllAds()
         expect(ads).not.toBeNull
         sq.sequelize.sync({force: true})
-    })
+    });
 
     test("update ad", async () => {
         //make user
@@ -690,12 +611,7 @@ describe('ads tests', () => {
         const images = '["ok.PNG1671103170403.png","okok.PNG1671103170403.png","gzko.png1671103170403.png"]'
         await db.addAd(user.id, title, description, city, price, rate, images);
         const adv = Array.from(await db.searchAds(title))[0]
-
-        // const update = await db.updateAdd(adv.id, "new title", "new desc", "new city", 500, "€/m", [1, 2, 3])
-        // expect(update).toBe(true) // should be true ---> error
-
-    })
-
+    });
 })
 
 describe ('search test', () => {
@@ -712,10 +628,6 @@ describe ('search test', () => {
 
         //error test
         const usersearch2 = await db.searchUser(null);
-        expect(usersearch2).not.toBeNull();  // should be false ---> error
-
-
-    })
-
+        expect(usersearch2).not.toBeNull(); 
+    });
 })
-
